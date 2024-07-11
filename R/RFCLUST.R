0291d61de2707b875c8dc6256dc2712430386f6f
borishejblum <boris.hejblum@gmail.com>
2024-07-12 00:34:10 +0200

    fixed rownames problems

--- R/RFCLUST.R
+++ R/RFCLUST.R
@@ -5,30 +5,51 @@
 #' Lie les fonctions ensemble j'espère mdr
 #'
 #' @param X a data.frame to be clustered
-#' @param n_trees number of \code{divlust} tress. Default is \code{500}.
+#' @param ntrees number of \code{divlust} tress. Default is \code{500}.
 #' @param K Nombre de cluster attendu par divclust.
 #' @param mtry Nombre de variables prises en comptes.
-
-#' @return la heat map et son dendrograme.
+#' @param ncores number of cpus to parallelize over. Default is
+#' \code{parallel::detectCores()-1}.
+#'
+#' @return a list of length \code{n_trees}, each elements containing 3 matrices.
 #'
 #' @importFrom pbapply pblapply
 #' @importFrom parallel detectCores
 #'
 #' @export
+#'
+#' @examples
+#' library(palmerpenguins)
+#' mypeng <- as.data.frame(penguins)
+#' mypeng$year <- as.factor(as.character(mypeng$year),
+#'                          levels=c("2007", "2008", "2009"),
+#'                          ordered=TRUE)
+#' set.seed(123)
+#' forest_clust <- rfclust(na.omit(mypeng[mypeng$sex=="male", -c(1, 7)]), ntrees = 50, ncores = 1)
+#'
+#' if(interactive()){
+#'   plot(forest_clust)
+#' }
+#'
 
 
 
-rfclust <- function(X, n_trees = 500, K = 2, mtry = 1, ncores = parallel::detectCores()-1){
+rfclust <- function(X, ntrees = 500, K = 2, mtry = 1, ncores = parallel::detectCores()-1){
 
 
   stopifnot(is.data.frame(X))
+
+  if(tibble::is_tibble(X)){
+    X <- as.data.frame(X)
+    warning("X is a tibble. It is converted to a simple `data.frame` instead.")
+  }
 
   if(is.null(rownames(X))){
     X <- as.character(1:nrow(X))
     message("No rownames for `X`. using row number instead.")
   }
 
-  forest <- pblapply(1:n_trees, function(i){
+  forest <- pblapply(1:ntrees, function(i){
     tree(X, K, mtry)
   }, cl = ncores)
 
--- R/tree.R
+++ R/tree.R
@@ -14,33 +14,33 @@
 
 tree <- function(X, K, mtry){
 
-
-  nb_col = ncol(X)
-  cut_var <- sample(1:nb_col, size = mtry)
-  X <- X[, cut_var, drop=F]                                               #Sample des variable
-
-  # if (mtry == 1){
-  #   X[, "Double"] <- X[,1]
-  # }                                                    #Divclust a besoin de 2 colonne
-
-  index_boot <- sample(1:nrow(X), size = nrow(X), replace = TRUE) # Bootstrap
-  X_ib <- X[index_boot, ]
-  X_oob  <- X[-index_boot,]
-
-
-  div <- divclust(X_ib, K)                                          # Divclust sur l'échantillon
+  cut_var <- sample(1:ncol(X), size = mtry)
+  #variable sample
+  Xtree <- X[, cut_var, drop=FALSE]
 
   rn <- rownames(X)
+  #observation bootstrap
+  index_boot <- sample(1:nrow(Xtree), size = nrow(Xtree), replace = TRUE)
+  oob <- rn[-unique(index_boot)]
+  X_ib <- Xtree[index_boot, , drop=FALSE]
+  #rownames(X_ib) <- make.unique(rn[index_boot])
+
+  # divclust tree on in-bag bootstrap sample
+  div <- divclust(X_ib, K)
+
 
   occu <- matrix(0, nrow(X), nrow(X), dimnames = list(rn, rn))                                     #Initialisation des matrices de stockages
   absent <- matrix(0, nrow(X), nrow(X), dimnames = list(rn, rn))
 
 
-  clus_indiv_unik <- sapply(div$clusters, function(x){unique(sapply(strsplit(x, ".", fixed = TRUE), "[", 1))})
+  clus_indiv_unik <- sapply(div$clusters,
+                            function(x){
+                              unique(sapply(strsplit(x, ".", fixed = TRUE), "[", 1))
+                            }
+  )
 
 
-  # On regarde dans chaque clusters
-
+  # Co-clustering occurences in each cluster
   for(k in 1:K){
     for(i in clus_indiv_unik[k]){
       for(j in clus_indiv_unik[k]){
@@ -50,13 +50,12 @@ tree <- function(X, K, mtry){
 
   }
 
-  # On regarde les OOB pour signifier les absentences.
-  for(Z in rownames(X_oob)){
-    absent[Z, ] <-  1                    # Toute les paires associé à l'individu qui est OOB sont donc absententes.
-    absent[, Z] <-  1
-  }
+  # On regarde les OOB pour signifier les absences.
+  # Toute les paires associé à l'individu qui est OOB sont donc absentes.
+  absent[oob, ] <-  1
+  absent[, oob] <-  1
 
-  diss <-  1 - absent - occu                                                    # Avec les absentences et les associations, facile de voir les paires dissociées.
+  diss <-  1 - absent - occu                                                    # Avec les absences et les associations, facile de voir les paires dissociées.
 
   # Retourne la liste des 3 matrices
   #( On s'intéresse surtout à absent pour la heatmap de la matrice de similarité.)
